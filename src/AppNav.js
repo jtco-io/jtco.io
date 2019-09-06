cae4371b09080a89afbddd501eb3de5f31248121
Nicholas Romero <ncrmro@jtco.io>
2019-09-05 19:30:39 -0500

    feat(styles): convert nav to use bootstrap

--- src/AppNav.js
+++ src/AppNav.js
@@ -1,50 +1,36 @@
 import React from 'react';
 import './AppNav.css';
 
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button'
+
 function AppNav() {
   return (
-    <nav className="AppNav">
-        <div>
-          <a href="/">
-            Home
-          </a>
-        </div>
-
-        <div>
-          <a href="/">
-            Home
-          </a>
-
-          <a href="/">
-            Services
-          </a>
-
-          <a href="/">
-            Contact
-          </a>
-
-          <a href="/">
-            News
-          </a>
-
-          <a href="/">
-            Investors
-          </a>
-
-          <a href="/">
-            About
-          </a>
-
-          <a href="/">
-            Team
-          </a>
-
-          <a href="/">
-            Consulting
-          </a>
-
-        </div>
-      </nav>
+    <Navbar>
+          <Navbar.Brand href="#home">jtco.io</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="home">Home</Nav.Link>
+              <Nav.Link href="link">Services</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+      </Navbar>
   );
 }
 
