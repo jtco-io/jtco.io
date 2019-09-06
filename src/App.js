ed17ee5af504121ca6e0ed684d00ab8a2451d943
Nicholas Romero <ncrmro@jtco.io>
2019-09-05 19:31:38 -0500

    feat(App): add initial jumbotron and row support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import AppNav from "./AppNav"
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <AppNav />
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+      <Jumbotron fluid>
+      <Container>
+        <h1>Fluid jumbotron</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a modified jumbotron that occupies the entire horizontal space of
+          its parent.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      </Container>
+    </Jumbotron>
+
+      <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
     </div>
   );
 }
