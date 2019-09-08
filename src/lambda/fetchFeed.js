a5f6669a85ba55e09c4c4e20aefc5e1028833e86
Nicholas Romero <ncrmro@jtco.io>
2019-09-08 02:11:04 -0500

    feat(Feed): add feed generator to lambda

--- src/lambda/fetchFeed.js
+++ src/lambda/fetchFeed.js
@@ -1,16 +1,36 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
-import axios from "axios";
+async function feedGenerator() {
+  function news(count = 10) {
+    let i = 0;
+    const items = [];
+    while (i < count) {
+      items.push({
+        title: "News today!",
+        body: "Todays body!"
+      });
+      i++;
+    }
+    return items;
+  }
+  return news();
+}
+
 export async function handler(event, context) {
   try {
-    const response = await axios.get("https://icanhazdadjoke.com", {
-      headers: { Accept: "application/json" }
-    });
-    const data = response.data;
+    // const response = await axios.get("https://icanhazdadjoke.com", {
+    //   headers: { Accept: "application/json" }
+    // });
+    // const data = response.data;
+
+    const articles = await feedGenerator(100)
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.joke })
+      body: JSON.stringify({
+        msg: "Successfully fetched Feed",
+        articles: articles
+      })
     };
   } catch (err) {
     console.log(err); // output to netlify function log
