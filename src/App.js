75241bb3a01a178cb4149b4c7335c3fd624ee06d
Nicholas Romero <ncrmro@jtco.io>
2019-09-07 20:40:07 -0500

    chore(prettier): initial commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,60 @@
-import React from 'react';
-import AppNav from "./AppNav"
-import NewsFeed from "./NewsFeed"
+import React from "react";
+import AppNav from "./AppNav";
+import NewsFeed from "./NewsFeed";
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <AppNav />
-        <Jumbotron fluid>
-          <Container>
-            <h1>Technology Forerunner</h1>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Technology Forerunner</h1>
+          <p>Rethinking what is possible with tomorrows ideas.</p>
+        </Container>
+      </Jumbotron>
+
+      <div className="Info">
+        <Row>
+          <Col sm>
+            <h3>Hardware, Networking, Software Design</h3>
             <p>
-              Rethinking what is possible with tomorrows ideas.
+              Let us improve your organizations process's with technical
+              solutions, find ways to provide better guarantees and while
+              keeping your users data secure.
             </p>
-          </Container>
-        </Jumbotron>
+          </Col>
 
-        <div className="Info">
-          <Row>
+          <Col sm>
+            <h3>Web Design, Marketing, Cloud Migrations</h3>
+            <p>
+              Let us improve your organizations process's with technical
+              solutions, find ways to provide better guarantees and while
+              keeping your users data secure.
+            </p>
+          </Col>
 
-              <Col sm>
-                <h3>Hardware, Networking, Software Design</h3>
-                <p>
-                  Let us improve your organizations process's with technical solutions,
-                find ways to provide better guarantees and while keeping your users data
-                secure.
-                </p>
-              </Col>
+          <Col sm>
+            <h3>
+              Solutions Architect, Reliability Engineering, Information Security
+            </h3>
 
-              <Col sm>
-                <h3>Web Design, Marketing, Cloud Migrations</h3>
-                <p>
-                  Let us improve your organizations process's with technical solutions,
-                find ways to provide better guarantees and while keeping your users data
-                secure.
-                </p>
-              </Col>
+            <p>
+              Let us improve your organizations process's with technical
+              solutions, find ways to provide better guarantees and while
+              keeping your users data secure.
+            </p>
+          </Col>
+        </Row>
 
-              <Col sm>
-                <h3>Solutions Architect, Reliability Engineering, Information Security</h3>
-
-                <p>
-                  Let us improve your organizations process's with technical solutions,
-                find ways to provide better guarantees and while keeping your users data
-                secure.
-                </p>
-              </Col>
-
-            </Row>
-
-            <NewsFeed/>
-        </div>
-
-
-
+        <NewsFeed />
+      </div>
     </div>
   );
 }
--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -1,42 +1,71 @@
-import React from 'react';
+import React from "react";
 // import './NewsFeed.css';
 
-import Row from 'react-bootstrap/Row';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 function NewsGenerator() {
-  function news(count=10) {
+  function news(count = 10) {
     let i = 0;
     const items = [];
     while (i < count) {
       items.push(
         <ListGroup.Item>
-              <Container>
-                <Row>
-                  <Col md={{ span: 6, offset: 3 }}>
-                    This is a singular news piece
-                  </Col>
-                </Row>
-              </Container>
+          <Container>
+            <Row>
+              <Col md={{ span: 6, offset: 3 }}>
+                This is a singular news piece
+              </Col>
+            </Row>
+          </Container>
         </ListGroup.Item>
-      )
+      );
       i++;
     }
-    return items
+    return items;
   }
-  return (
-    <ListGroup variant="flush" >{news()}</ListGroup>
-  )
+  return <ListGroup variant="flush">{news()}</ListGroup>;
 }
 
-function NewsFeed() {
-  return (
-    <div className="News">
-      <NewsGenerator/>
-    </div>
-  );
+class NewsFeed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: null };
+  }
+
+  handleClick = api => e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+  };
+
+  render() {
+    const { loading, msg } = this.state;
+
+    return (
+      <div>
+        <p>
+          <button onClick={this.handleClick("hello")}>
+            {loading ? "Loading..." : "Call Lambda"}
+          </button>
+          <button onClick={this.handleClick("fetchFeed")}>
+            {loading ? "Loading..." : "Call Async Lambda"}
+          </button>
+          <br />
+          <span>{msg}</span>
+        </p>
+
+        <div className="News">
+          <NewsGenerator />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NewsFeed;
