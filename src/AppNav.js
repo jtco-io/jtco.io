aaa3bc9bea13a40085ca1e204fad6558290cbdbe
Nicholas Romero <ncrmro@jtco.io>
2019-09-08 15:17:17 -0500

    feat(Router): add bootstrap link support

--- src/AppNav.js
+++ src/AppNav.js
@@ -9,6 +9,8 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
+import { LinkContainer } from "react-router-bootstrap"
+
 function AppNav() {
   return (
     <Navbar>
@@ -16,8 +18,15 @@ function AppNav() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="home">Home</Nav.Link>
-          <Nav.Link href="link">Services</Nav.Link>
+
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+
+          <LinkContainer to="/services">
+            <Nav.Link>Services</Nav.Link>
+          </LinkContainer>
+
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
