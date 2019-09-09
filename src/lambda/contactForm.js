5d0d27c7f310968b82a69f4ff3956505cfa46e11
Nicholas Romero <ncrmro@jtco.io>
2019-09-09 06:34:47 -0500

    feat(Contact): add initial lambda support

---
+++ src/lambda/contactForm.js
@@ -0,0 +1,35 @@
+export async function handler(event, context) {
+  var nodemailer = require('nodemailer');
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'youremail@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
+
+  var mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'myfriend@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  try {
+    let info = await transporter.sendMail(mailOptions);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: `Successfully sent email ${info.response}`
+      })
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+}
