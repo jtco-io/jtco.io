2b9812d39942f396a2c44f5dadbc7471c3eece18
Nicholas Romero <ncrmro@jtco.io>
2019-09-09 05:49:41 -0500

    feat(Projects): use grid mosaic

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,61 +2,59 @@ import React from "react";
 
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-// import "./Projects.css";
+import { GridMosaic } from "../components/GridMosaic"
 
 function fakeProjects() {
   const projects = []
   let times = 10
   for(var i=0; i < times; i ++) {
     projects.push({
+      uuid: i,
       organization: "",
-      title: "",
+      title: `Project Title ${i}`,
       tags: "",
       body: "",
     })
   }
+  return projects
+}
+function ProjectCard(proj) {
+  return (
+    <Card>
+      <Card.Header as="h5">Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>{proj.title}</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  )
 }
 
 function Projects() {
+
+  const projects = fakeProjects();
+
+  projects.forEach(
+    (project, key) => projects[key] = ProjectCard(projects[key])
+  )
+
   return (
     <div className="Projects">
       <Jumbotron fluid>
         <Container>
           <h1>Projects</h1>
-          <p>
-            Ways we enable communities to better foster success and share
-            opportunity.
-          </p>
         </Container>
       </Jumbotron>
 
-      <div className="Info">
-        <Row>
-          <Col sm>
-            <h3>Opensource</h3>
-            <p>
-              * rjango
-
-            </p>
-          </Col>
-
-          <Col sm>
-            <h3>Startups</h3>
-            <p>
-              Passing on concepts and recipes to foster healthier communities.
-            </p>
-          </Col>
-
-          <Col sm>
-            <h3>Previous Work</h3>
-
-            <p>Allowing others to make their own journey</p>
-          </Col>
-        </Row>
-      </div>
+      <Container>
+        {GridMosaic(projects)}
+      </Container>
     </div>
   );
 }
