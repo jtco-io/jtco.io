8b68f9d1e8eb5cfd5b86737b5bd47db4f4510b63
Nicholas Romero <ncrmro@jtco.io>
2019-09-16 21:37:05 -0500

    feat(Projects): make working links

--- src/pages/ProjectCards.js
+++ src/pages/ProjectCards.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-import { GridMosaic } from "../components/GridMosaic"
+import { LinkContainer } from "react-router-bootstrap"
 
 import "./ProjectCards.css"
 
@@ -14,7 +14,12 @@ export function PcJTX(proj) {
       <Card>
         <Card.Img variant="top" src="proj_jtx.png" />
         <Card.Body>
-          <Card.Title>jtronics.exchange</Card.Title>
+
+          <Card.Title>
+            <a href="https://jtronics.exchange">jtronics.exchange</a>
+          </Card.Title>
+
+
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in to
             additional content. This content is a little bit longer.
@@ -29,7 +34,13 @@ export function PcFE(proj) {
       <Card>
         <Card.Img variant="top" src="proj_fe.png" />
         <Card.Body>
-          <Card.Title>Floral Events</Card.Title>
+
+
+          <Card.Title>
+            <a href="http://floralevents.com">Floral Events</a>
+          </Card.Title>
+
+
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in to
             additional content. This content is a little bit longer.
